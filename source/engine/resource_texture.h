0cdbf1d4bdd8e16b020333e7cfb4664d3eef8988
aroille <antoine.roille@live.fr>
2014-11-24 20:44:31 +1100

    fix header

--- source/engine/resource_texture.h
+++ source/engine/resource_texture.h
@@ -1,11 +1,10 @@
-#ifndef RESOURCE_LOADER_H_HEADER_GUARD
-#define RESOURCE_LOADER_H_HEADER_GUARD
+#ifndef RESOURCE_TEXTURE_H_HEADER_GUARD
+#define RESOURCE_TEXTURE_H_HEADER_GUARD
 
 #include "resource_manager.h"
 
 namespace loco
 {
-
 
 	template<> Texture ResourceManager::create(const Memory* mem) const
 	{
@@ -24,4 +23,4 @@ namespace loco
 	}
 }
 
-#endif // RESOURCE_LOADER_H_HEADER_GUARD
+#endif // RESOURCE_TEXTURE_H_HEADER_GUARD
