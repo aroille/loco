c057d44f1edac468dda0c13187d1d90f06bea31c
aroille <antoine.roille@live.fr>
2014-11-25 18:41:19 +1100

    add hash_string function

--- source/core/murmur_hash.h
+++ source/core/murmur_hash.h
@@ -1,8 +1,10 @@
 #ifndef MURMUR_HASH_H_HEADER_GUARD
 #define MURMUR_HASH_H_HEADER_GUARD
 
-#include <stdint.h>
 #include "defines.h"
+
+#include <stdint.h>
+#include <string>
 
 namespace loco
 {
@@ -11,6 +13,11 @@ namespace loco
 	/// Implementation of the 64 bit MurmurHash2 function
 	/// http://murmurhash.googlepages.com/
 	uint64_t murmur_hash_64(const void *key, uint32_t len, uint64_t seed = LOCO_MURMUR_HASH_SEED);
+
+	inline HashedString hash_string(const char* string)
+	{
+		return murmur_hash_64((void*)string, strlen(string));
+	}
 }
 
 #endif // MURMUR_HASH_H_HEADER_GUARD
